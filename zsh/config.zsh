6fa7bee1029536165f1e7bca1229cbae6fe2b512
opticXC <aabishmalik018@gmail.com>
2023-08-27 20:15:23 +0000

    complete exchange1

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -11,14 +11,14 @@ promptinit
 
 
 # Setting up promptinit
-promptinit suse
+# promptinit suse
 
 # Starship
-# source <(/sbin/starship init zsh --print-full-init)
+source <(/sbin/starship init zsh --print-full-init)
 
 
 # Find The Command
-# source /usr/share/doc/find-the-command/ftc.zsh
+source /usr/share/doc/find-the-command/ftc.zsh
 
 
 # Common Aliases
@@ -26,6 +26,50 @@ alias ls='exa -al --color=always --group-directories-first --icons'
 
 alias cat='bat --style header --style snip --style changes --style header'
 
+
+
+
+## Setting up Key Commands
+#
+typeset -g -A key
+
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Insert]="${terminfo[kich1]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+
+# setup key accordingly
+[[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
+[[ -n "${key[End]}"       ]] && bindkey -- "${key[End]}"        end-of-line
+#[[ -n "${key[Insert]}"    ]] && bindkey -- "${key[Insert]}"     overwrite-mode
+[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}"  backward-delete-char
+[[ -n "${key[Delete]}"    ]] && bindkey -- "${key[Delete]}"     delete-char
+[[ -n "${key[Up]}"        ]] && bindkey -- "${key[Up]}"         up-line-or-history
+[[ -n "${key[Down]}"      ]] && bindkey -- "${key[Down]}"       down-line-or-history
+[[ -n "${key[Left]}"      ]] && bindkey -- "${key[Left]}"       backward-char
+[[ -n "${key[Right]}"     ]] && bindkey -- "${key[Right]}"      forward-char
+[[ -n "${key[PageUp]}"    ]] && bindkey -- "${key[PageUp]}"     beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}"  ]] && bindkey -- "${key[PageDown]}"   end-of-buffer-or-history
+[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}"  reverse-menu-complete
+
+
+# Activate Key Commands if terminal is in application mode
+# terminfo is only valid in application mode
+if (( ${+terminfo[smkx]} && ${+terminfo[rmks]} )); then
+	autoload -Uz add-zle-hook-widget
+	function zle_application_mode_start { echoti smkx }
+	function zle_application_mode_stop { echoti rmkx }
+	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
 
 
 
