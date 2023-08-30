b6a2e120bbf682beb32d2dfbdacaf37c623b2ee7
opticXC <aabishmalik018@gmail.com>
2023-08-30 16:53:34 +0530

    changed default browser to chromium added colorpicker options to the screenshot menu using hyprpicker added azote

--- sway/scripts/screenshot.sh
+++ sway/scripts/screenshot.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-entries="Active Screen Output Area Window"
+entries="Area ColorPicker  Window Screen"
 
 selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
 
 case $selected in
-  active)
-    /usr/share/swayfx/scripts/grimshot --notify save active;;
   screen)
     /usr/share/swayfx/scripts/grimshot --notify save screen;;
-  output)
-    /usr/share/swayfx/scripts/grimshot --notify save output;;
   area)
     /usr/share/swayfx/scripts/grimshot --notify save area;;
   window)
     /usr/share/swayfx/scripts/grimshot --notify save window;;
+  colorpicker)
+    /usr/bin/hyp#E0877Brpicker --no-fancy --autocopy --format=hex;;
 esac
