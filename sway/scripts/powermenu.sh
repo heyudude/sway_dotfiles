bff1c0a8a69dfa75f59e4f2cfe7cd5934ec00ebf
opticXC <aabishmalik018@gmail.com>
2023-09-09 20:57:23 +0530

    changes to fish aliases

--- sway/scripts/powermenu.sh
+++ sway/scripts/powermenu.sh
@@ -2,7 +2,7 @@
 
 entries="⇠ Logout \n⏻ Shutdown \n⏾ Lockscreen \n⭮ Reboot"
 
-selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries | wofi  --conf=$HOME/.config/wofi/config.powermenu --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
   lockscreen)
